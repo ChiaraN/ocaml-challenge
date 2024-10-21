7aa30fa65e72d6c6fc3e6dae5a3c22a05e9eb2d2
ChiaraN <chiaradettaclaire@gmail.com>
2024-10-21 17:06:46 +0200

    Esercizi Multiplexer

--- 1/nand/Es_3_level_1.ml
+++ 1/nand/Es_3_level_1.ml
@@ -10,12 +10,25 @@ let mux2 (s0 : bool) (a : bool) (b : bool) : bool =
   match s0 with
   |true -> a
   | false -> b
+;;
 
-assert (mux2_1 true true true = true);;
-assert (mux2_1 true true false = true);;
-assert (mux2_1 true false true= false);;
-assert (mux2_1 true false false = false);;
-assert (mux2_1 false true true = true);;
-assert (mux2_1 false true false = false);;
-assert (mux2_1 false false true = true);;
-assert (mux2_1 false false false = false);;
+
+
+assert (mux2 true true true = true);;
+assert (mux2 true true false = true);;
+assert (mux2 true false true= false);;
+assert (mux2 true false false = false);;
+assert (mux2 false true true = true);;
+assert (mux2 false true false = false);;
+assert (mux2 false false true = true);;
+assert (mux2 false false false = false);;
+
+
+let mux4 (s0: bool) (s1 :bool) (a: bool) (b: bool) (c: bool) (d: bool) =
+  mux2 s0 (mux2 s1 d c) (mux2 s0 a b) = mux2 s0 (mux2 s1 d c) (mux2 s1 b a)
+;;
+
+assert(mux4 false false false true false true = false);;
+assert(mux4 false true false true false true = true);;
+assert(mux4 true false false true false true = false);;
+assert(mux4 true true false true false true = true);;
