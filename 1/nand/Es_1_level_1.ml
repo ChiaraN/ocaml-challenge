db1fe68e51af93c96c8c082c04fa788161eeae47
ChiaraN <chiaradettaclaire@gmail.com>
2024-11-08 12:40:07 +0100

    Es_Logical_Nand

--- 1/nand/Es_1_level_1.ml
+++ 1/nand/Es_1_level_1.ml
@@ -1,6 +1,12 @@
 let f1 p1 p2 = not (p1 && p2);;
 
-let f2 b1 b2 = if b1 than b2 else b1 * b2 ;;
+let f2 (b1 : bool) (b2 : bool) : bool = 
+    if b1 
+    then 
+        if b2 
+            then false 
+        else true 
+    else true;;
 
 let f3 c1 c2 = match (c1, c2) with 
     | (false, false) -> true 
